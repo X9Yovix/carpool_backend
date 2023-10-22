111bfafff0c73432397f308421927ae2524b4c08
Karim Ouazzou <karimouazzou@gmail.com>
2023-10-22 18:18:10 +0100

    first commit

---
+++ src/main/java/com/tekup/carpool_backend/CarpoolAppApplication.java
@@ -0,0 +1,13 @@
+package com.tekup.carpool_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolAppApplication.class, args);
+	}
+
+}
