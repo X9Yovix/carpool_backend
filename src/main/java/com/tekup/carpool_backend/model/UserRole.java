0cbaa330d349fb7bef3f6d9cea85b653f47e4c95
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-02 20:13:16 +0100

    User model

---
+++ src/main/java/com/tekup/carpool_backend/model/UserRole.java
@@ -0,0 +1,7 @@
+package com.tekup.carpool_backend.model;
+
+public enum UserRole {
+    ADMIN,
+    DRIVER,
+    PASSENGER
+}
--- src/main/java/com/tekup/carpool_backend/repository/UserRepository.java
+++ src/main/java/com/tekup/carpool_backend/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.tekup.carpool_backend.repository;
 
-public interface UserRepository {
+import com.tekup.carpool_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
