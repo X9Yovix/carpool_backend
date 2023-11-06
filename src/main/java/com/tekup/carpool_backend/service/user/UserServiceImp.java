939721432bd4e74e70461b76ca127ef9471d44df
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-06 16:18:55 +0100

    adding 'verified' to initial users

--- src/main/java/com/tekup/carpool_backend/service/user/UserServiceImp.java
+++ src/main/java/com/tekup/carpool_backend/service/user/UserServiceImp.java
@@ -21,9 +21,9 @@ public class UserServiceImp implements UserService {
     private final PasswordEncoder passwordEncoder;
 
     public boolean seedInitialUsers() {
-        User admin1 = new User(1L, "Ali", "Ben Ali", "admin@gmail.com", BCrypt.hashpw("adminpassword", BCrypt.gensalt()), UserRole.ADMIN);
-        User driver1 = new User(2L, "Saleh", "Ben Saleh", "driver@gmail.com", BCrypt.hashpw("driverpassword", BCrypt.gensalt()), UserRole.DRIVER);
-        User passenger1 = new User(3L, "Mohamed", "Ben Mohamed", "passenger@gmail.com", BCrypt.hashpw("passengerpassword", BCrypt.gensalt()), UserRole.PASSENGER);
+        User admin1 = new User(1L, "Ali", "Ben Ali", "admin@gmail.com", BCrypt.hashpw("adminpassword", BCrypt.gensalt()), UserRole.ADMIN, true);
+        User driver1 = new User(2L, "Saleh", "Ben Saleh", "driver@gmail.com", BCrypt.hashpw("driverpassword", BCrypt.gensalt()), UserRole.DRIVER,true);
+        User passenger1 = new User(3L, "Mohamed", "Ben Mohamed", "passenger@gmail.com", BCrypt.hashpw("passengerpassword", BCrypt.gensalt()), UserRole.PASSENGER,true);
 
         List<User> savedUsers = userRepository.saveAll(List.of(admin1, driver1, passenger1));
         return !savedUsers.isEmpty();
